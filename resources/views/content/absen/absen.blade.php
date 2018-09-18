35462771c4b39e34b641cdac7e10840d2b93202e
bagas <bagassigit24@gmail.com>
2018-09-18 16:22:46 +0700

    tes commit

--- resources/views/content/absen/absen.blade.php
+++ resources/views/content/absen/absen.blade.php
@@ -7,7 +7,7 @@
 <section class="content-header">
   <h1>
     Absen
-    <small>Optional description</small>
+    <small>Optional dasdas</small>
   </h1>
   <ol class="breadcrumb">
     <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
